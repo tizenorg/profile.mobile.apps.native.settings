412934f95cb178c8c2ded92c1b43573aa80f6163
Bartlomiej Uliasz <b.uliasz@samsung.com>
2016-04-26 14:09:40 +0200

    Made setting-flightmode as application
    Change-Id: Ibdafbfc23e1e448d1cc8e8f2847e3ae825ced357
    Signed-off-by: Bartlomiej Uliasz <b.uliasz@samsung.com>

--- setting-flightmode/src/setting-flightmode.c
+++ setting-flightmode/src/setting-flightmode.c
@@ -18,206 +18,126 @@
  * limitations under the License.
  *
  */
-#include <setting-flightmode.h>
 
-#ifndef UG_MODULE_API
-#define UG_MODULE_API __attribute__ ((visibility("default")))
-#endif
+#include "setting-debug.h"
+#include "setting-flightmode.h"
 
-static void setting_flightmode_ug_cb_resize(void *data, Evas *e,
-											Evas_Object *obj, void *event_info)
+#include <app.h>
+#include <appcore-common.h>
+
+
+#define SETTING_FLIGHTMODE_PACKAGE_NAME "org.tizen.setting-flightmode"
+
+
+static void setting_flightmode_cb_resize(void *data, Evas *e,
+		Evas_Object *obj, void *event_info)
 {
-	SettingFlightModeUG *ad = (SettingFlightModeUG *) data;
+	SettingFlightMode *ad = (SettingFlightMode *) data;
 	setting_view_update(&setting_view_flightmode_main, ad);
 }
 
-setting_view *__get_flightmode_view_to_load(void *data, app_control_h service)
+static bool app_create(void *data)
 {
 	SETTING_TRACE_BEGIN;
-	setting_retvm_if((!data), NULL, "data is NULL");
+	SettingFlightMode *ad = (SettingFlightMode *) data;
 
-	setting_view_node_table_intialize();
+	setting_retvm_if((!ad), NULL, "!priv");
 
-	/* default action */
-	setting_view_node_table_register(&setting_view_flightmode_main, NULL);
-	return &setting_view_flightmode_main;
+	setting_set_i18n(SETTING_PACKAGE, FLIGHTMODE_LOCALEDIR);
+
+	if(app_init(&ad->md, SETTING_FLIGHTMODE_PACKAGE_NAME)
+			!= SETTING_RETURN_SUCCESS) {
+		SETTING_TRACE_ERROR("Cannot initialize application");
+		return false;
+	}
+
+//	setting_create_Gendial_itc("dialogue/1text.2icon",
+//			&(ad->itc_1text_2icon));
+//
+//	/* creating a view. */
+//	setting_view_node_set_cur_view(ad->view_to_load);
+//
+//	/* register view node table */
+//	setting_view_node_table_intialize();
+//	setting_view_node_table_register(&setting_view_flightmode_main, NULL);
+//	ad->view_to_load = &setting_view_flightmode_main;
+	setting_view_create(&setting_view_flightmode_main, ad);
+
+	evas_object_event_callback_add(ad->md.win_main,
+					EVAS_CALLBACK_RESIZE,
+					setting_flightmode_cb_resize, ad);
+
+	SETTING_TRACE_END;
+	return true;
 }
 
-static void *setting_flightmode_ug_on_create(ui_gadget_h ug,
-											 enum ug_mode mode, app_control_h service,
-											 void *priv)
+static void app_reset(app_control_h service, void *data)
+{
+}
+
+static void app_pause(void *data)
+{
+}
+
+static void app_resume(void *data)
+{
+}
+
+static void app_terminate(void *data)
 {
 	SETTING_TRACE_BEGIN;
-	setting_retvm_if((!priv), NULL, "!priv");
-	SettingFlightModeUG *flightmodeUG = priv;
-	flightmodeUG->ug = ug;
-	flightmodeUG->bundle_data = service;
-	setting_set_i18n(SETTING_PACKAGE, SETTING_LOCALEDIR);
+	SettingFlightMode *priv = data;
 
-	flightmodeUG->win_main_layout = (Evas_Object *) ug_get_parent_layout(ug);
-	flightmodeUG->win_get = (Evas_Object *) ug_get_window();
-
-	evas_object_show(flightmodeUG->win_main_layout);
-	flightmodeUG->evas = evas_object_evas_get(flightmodeUG->win_main_layout);
-
-	setting_retvm_if(flightmodeUG->win_main_layout == NULL, NULL,
-					 "cannot get main window ");
-
-	/* register view node table */
-	flightmodeUG->view_to_load = __get_flightmode_view_to_load(flightmodeUG, service);
-	setting_retvm_if(NULL == flightmodeUG->view_to_load, NULL, "NULL == flightmodeUG->view_to_load");
-
-	setting_create_Gendial_itc("dialogue/1text.2icon", &(flightmodeUG->itc_1text_2icon));
-
-	/* creating a view. */
-	setting_view_node_set_cur_view(flightmodeUG->view_to_load);
-	setting_view_create(flightmodeUG->view_to_load, (void *)flightmodeUG);
-
-	evas_object_event_callback_add(flightmodeUG->win_main_layout,
-								   EVAS_CALLBACK_RESIZE,
-								   setting_flightmode_ug_cb_resize, flightmodeUG);
-
-	/*---------------------------------------------------------------- */
-	/* add genlist */
-	/*---------------------------------------------------------------- */
-	return flightmodeUG->ly_main;
-}
-
-static void setting_flightmode_ug_on_start(ui_gadget_h ug, app_control_h service,
-										   void *priv)
-{
-}
-
-static void setting_flightmode_ug_on_pause(ui_gadget_h ug, app_control_h service,
-										   void *priv)
-{
-}
-
-static void setting_flightmode_ug_on_resume(ui_gadget_h ug, app_control_h service,
-											void *priv)
-{
-}
-
-static void setting_flightmode_ug_on_destroy(ui_gadget_h ug, app_control_h service,
-											 void *priv)
-{
-	SETTING_TRACE_BEGIN;
 	setting_retm_if((!priv), "!priv");
-	SettingFlightModeUG *flightmodeUG = priv;
-	evas_object_event_callback_del(flightmodeUG->win_main_layout, EVAS_CALLBACK_RESIZE, setting_flightmode_ug_cb_resize);	/*fix flash issue for gallery */
-	flightmodeUG->ug = ug;
-	if (flightmodeUG->bg) {
-		evas_object_del(flightmodeUG->bg);
-	}
-	flightmodeUG->bg = NULL;
+	/*fix flash issue for gallery */
+	evas_object_event_callback_del(priv->md.win_main, EVAS_CALLBACK_RESIZE,
+					setting_flightmode_cb_resize);
+	if (priv->bg)
+		evas_object_del(priv->bg);
+	priv->bg = NULL;
 
-	/* called when this shared gadget is terminated. similar with app_exit */
-	setting_view_destroy(&setting_view_flightmode_main, flightmodeUG);
-
-	if (NULL != ug_get_layout(flightmodeUG->ug)) {
-		evas_object_hide((Evas_Object *) ug_get_layout(flightmodeUG->ug));
-		evas_object_del((Evas_Object *) ug_get_layout(flightmodeUG->ug));
-	}
+	/* called when this shared gadget is terminated. similar to app_exit */
+	setting_view_destroy(&setting_view_flightmode_main, priv);
 }
 
-static void setting_flightmode_ug_on_message(ui_gadget_h ug, app_control_h msg,
-											 app_control_h service, void *priv)
+static void _lang_changed(app_event_info_h event_info, void *data)
 {
-	SETTING_TRACE_BEGIN;
-}
-
-static void setting_flightmode_ug_on_event(ui_gadget_h ug,
-										   enum ug_event event, app_control_h service,
-										   void *priv)
-{
-	SETTING_TRACE_BEGIN;
-	switch (event) {
-	case UG_EVENT_LOW_MEMORY:
-		break;
-	case UG_EVENT_LOW_BATTERY:
-		break;
-	case UG_EVENT_LANG_CHANGE:
-		break;
-	case UG_EVENT_ROTATE_PORTRAIT:
-		break;
-	case UG_EVENT_ROTATE_PORTRAIT_UPSIDEDOWN:
-		break;
-	case UG_EVENT_ROTATE_LANDSCAPE:
-		break;
-	case UG_EVENT_ROTATE_LANDSCAPE_UPSIDEDOWN:
-		break;
-	case UG_EVENT_REGION_CHANGE:
-		break;
-	default:
-		break;
+	char *lang = NULL;
+	if (app_event_get_language(event_info, &lang) == APP_ERROR_NONE) {
+		SETTING_TRACE_DEBUG("Setting - language is changed : %s", lang);
+		elm_language_set(lang);
+		elm_config_all_flush();
+		free(lang);
+	} else {
+		SETTING_TRACE_ERROR("Cannot get language from event_info");
 	}
 }
 
-static void setting_flightmode_ug_on_key_event(ui_gadget_h ug,
-											   enum ug_key_event event,
-											   app_control_h service, void *priv)
+EXPORT_PUBLIC
+int main(int argc, char *argv[])
 {
-	SETTING_TRACE_BEGIN;
-	SettingFlightModeUG *ad = (SettingFlightModeUG *) priv;
+	app_event_handler_h handlers[5] = {NULL, };
+	ui_app_lifecycle_callback_s ops = {
+		.create = app_create,
+		.pause = app_pause,
+		.resume = app_resume,
+		.terminate = app_terminate,
+		.app_control = app_reset,
+	};
+	SettingFlightMode app_data;
 
-	switch (event) {
-	case UG_KEY_EVENT_END: {
-			if (elm_naviframe_top_item_get(ad->navi_bar) ==
-				elm_naviframe_bottom_item_get(ad->
-											  navi_bar)) {
-				ug_destroy_me(ug);
-			} else {
+	ui_app_add_event_handler(&handlers[APP_EVENT_LOW_MEMORY],
+			APP_EVENT_LOW_MEMORY, NULL, NULL);
+	ui_app_add_event_handler(&handlers[APP_EVENT_LOW_BATTERY],
+			APP_EVENT_LOW_BATTERY, NULL, NULL);
+	ui_app_add_event_handler(&handlers[APP_EVENT_LANGUAGE_CHANGED],
+			APP_EVENT_LANGUAGE_CHANGED, _lang_changed, NULL);
+	ui_app_add_event_handler(&handlers[APP_EVENT_REGION_FORMAT_CHANGED],
+			APP_EVENT_REGION_FORMAT_CHANGED, NULL, NULL);
+	ui_app_add_event_handler(
+			&handlers[APP_EVENT_DEVICE_ORIENTATION_CHANGED],
+			APP_EVENT_DEVICE_ORIENTATION_CHANGED, NULL, NULL);
 
-				setting_view_cb_at_endKey(ad);
-			}
-		}
-		break;
-	default:
-		break;
-	}
+	memset(&app_data, 0x0, sizeof(app_data));
+	return ui_app_main(argc, argv, &ops, &app_data);
 }
-
-UG_MODULE_API int UG_MODULE_INIT(struct ug_module_ops *ops)
-{
-	SETTING_TRACE_BEGIN;
-	SettingFlightModeUG *flightmodeUG = calloc(1, sizeof(SettingFlightModeUG));
-	setting_retvm_if(!flightmodeUG, -1, "Create SettingFlightModeUG obj failed");
-
-	ops->create = setting_flightmode_ug_on_create;
-	ops->start = setting_flightmode_ug_on_start;
-	ops->pause = setting_flightmode_ug_on_pause;
-	ops->resume = setting_flightmode_ug_on_resume;
-	ops->destroy = setting_flightmode_ug_on_destroy;
-	ops->message = setting_flightmode_ug_on_message;
-	ops->event = setting_flightmode_ug_on_event;
-	ops->key_event = setting_flightmode_ug_on_key_event;
-	ops->priv = flightmodeUG;
-	ops->opt = UG_OPT_INDICATOR_ENABLE;
-	return 0;
-}
-
-UG_MODULE_API void UG_MODULE_EXIT(struct ug_module_ops *ops)
-{
-	SETTING_TRACE_BEGIN;
-	struct SettingFlightModeUG *flightmodeUG;
-	setting_retm_if(!ops, "ops == NULL");
-
-	flightmodeUG = ops->priv;
-	if (flightmodeUG) {
-		FREE(flightmodeUG);
-	}
-}
-
-/* ***************************************************
-**
-** general func
-**
-** **************************************************/
-
-/**
-* Reset function to 'reset' the settings of the UG, it will be invoked by 'Reset' UG
-*
-* @param[in] data
-* @param[in] priv
-*/
-
