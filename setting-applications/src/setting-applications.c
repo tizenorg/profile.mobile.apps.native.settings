ca57732957f1fc20b53fa96686fe3a0d55ee137a
Bartlomiej Uliasz <b.uliasz@samsung.com>
2016-07-11 10:45:41 +0200

    Applications UG module to app_launcher conversion
    Change-Id: I87d1532611a2dd7ee052a7274ee42ef91f48b7f6
    Signed-off-by: Bartlomiej Uliasz <b.uliasz@samsung.com>

--- setting-applications/src/setting-applications.c
+++ setting-applications/src/setting-applications.c
@@ -20,10 +20,11 @@
  */
 #include <appfw/app_control_internal.h>
 #include <Eina.h>
-
 #include <setting-applications.h>
 #include <setting-cfg.h>
 
+#define SETTING_APPLICATIONS_PACKAGE_NAME "org.tizen.setting-applications"
+#define APPLICATIONS_LOCALEDIR _TZ_SYS_RO_APP"/org.tizen.setting-applications/res/locale"
 #define SETTING_BRIGHTNESS_DEFAULT_LEVEL 60
 
 #ifndef UG_MODULE_API
@@ -36,7 +37,7 @@
  * viewtype == 'brightness' --> setting_view_applications_brightness
  * viewtype != NULL && viewtype --> setting_view_applications_main
  */
-setting_view *__get_applications_view_to_load(void *data, app_control_h service)
+setting_view *__get_applications_view_to_load(void *data)
 {
 	SETTING_TRACE_BEGIN;
 	setting_retvm_if((!data), NULL, "!data");
@@ -57,7 +58,7 @@ Evas_Object *__get_applications_layout_to_return(app_control_h service,
 {
 	SETTING_TRACE_BEGIN;
 	SettingApplicationsUG *applicationsUG = priv;
-	return applicationsUG->ly_main;
+	return applicationsUG->md.ly_main;
 
 }
 
@@ -181,88 +182,69 @@ Evas_Object *_gl_1button1_icon_get(void *data, Evas_Object *obj,
 		return NULL;
 }
 
-static void *setting_applications_ug_on_create(ui_gadget_h ug,
-		enum ug_mode mode, app_control_h service, void *priv)
+static bool app_create(void *priv)
 {
-	setting_retvm_if((!priv), NULL, "!priv");
 	SETTING_TRACE_BEGIN;
+	SettingApplicationsUG *ad = priv;
+	setting_retvm_if((!priv), NULL, "!priv");
 
-	SettingApplicationsUG *applicationsUG = priv;
-	applicationsUG->ug = ug;
-	setting_set_i18n(SETTING_PACKAGE, SETTING_LOCALEDIR);
+	setting_set_i18n(SETTING_PACKAGE, APPLICATIONS_LOCALEDIR);
 
-	applicationsUG->win_main_layout = (Evas_Object *)ug_get_parent_layout(
-			ug);
-	applicationsUG->win_get = (Evas_Object *)ug_get_window();
-
-	/*evas_object_show(applicationsUG->win_main_layout); */
-	applicationsUG->evas = evas_object_evas_get(
-			applicationsUG->win_main_layout);
-
-	setting_retvm_if(applicationsUG->win_main_layout == NULL, NULL,
-			"cannot get main window ");
+	if (app_init(&ad->md, SETTING_APPLICATIONS_PACKAGE_NAME)
+			!= SETTING_RETURN_SUCCESS) {
+		SETTING_TRACE_ERROR("Cannot initialize application");
+		return false;
+	}
 
 	/* register view node table */
 	setting_view_node_table_intialize();
 
 	setting_create_Gendial_itc(SETTING_GENLIST_2LINE_STYLE,
-			&(applicationsUG->itc_2text_2));
+			&(ad->itc_2text_2));
 	setting_create_Gendial_itc(SETTING_GENLIST_ICON_1LINE_STYLE,
-			&(applicationsUG->itc_1text_1icon));
+			&(ad->itc_1text_1icon));
 	setting_create_Gendial_itc(SETTING_GENLIST_ICON_1LINE_STYLE,
-			&(applicationsUG->itc_1icon_1text_sub));
+			&(ad->itc_1icon_1text_sub));
 	setting_create_Gendial_itc(SETTING_GENLIST_ICON_1LINE_STYLE,
-			&(applicationsUG->itc_1text));
+			&(ad->itc_1text));
 	setting_create_Gendial_itc(SETTING_GENLIST_ICON_1LINE_STYLE,
-			&(applicationsUG->itc_2text_3));
+			&(ad->itc_2text_3));
 	setting_create_Gendial_itc(SETTING_GENLIST_ICON_1LINE_STYLE,
-			&(applicationsUG->itc_1text_1icon_divider));
+			&(ad->itc_1text_1icon_divider));
 	setting_create_Gendial_itc(SETTING_GENLIST_GROUP_INDEX_STYLE,
-			&(applicationsUG->itc_grp_title));
+			&(ad->itc_grp_title));
 
-	applicationsUG->itc_1icon_1button.item_style = SETTING_GENLIST_2LINE_STYLE;
-	applicationsUG->itc_1icon_1button.func.text_get = _gl_label_new_get;
-	applicationsUG->itc_1icon_1button.func.content_get =
+	ad->itc_1icon_1button.item_style = SETTING_GENLIST_2LINE_STYLE;
+	ad->itc_1icon_1button.func.text_get = _gl_label_new_get;
+	ad->itc_1icon_1button.func.content_get =
 			_gl_1button1_icon_get;
-	applicationsUG->itc_1icon_1button.func.state_get = NULL;
-	applicationsUG->itc_1icon_1button.func.del = NULL;
-
+	ad->itc_1icon_1button.func.state_get = NULL;
+	ad->itc_1icon_1button.func.del = NULL;
 
 	/*	creating a view. */
-	applicationsUG->view_to_load = __get_applications_view_to_load(
-			applicationsUG, service);
-	setting_retvm_if(NULL == applicationsUG->view_to_load, NULL,
+	ad->view_to_load = __get_applications_view_to_load(ad);
+	setting_retvm_if(NULL == ad->view_to_load, NULL,
 			"NULL == applicationsUG->view_to_load");
 
-	setting_view_node_set_cur_view(applicationsUG->view_to_load);
-	setting_view_create(applicationsUG->view_to_load,
-			(void *)applicationsUG);
-	evas_object_event_callback_add(applicationsUG->win_main_layout,
-			EVAS_CALLBACK_RESIZE, setting_applications_ug_cb_resize,
-			applicationsUG);
-	return __get_applications_layout_to_return(service, applicationsUG);
+	setting_view_node_set_cur_view(ad->view_to_load);
+	setting_view_create(ad->view_to_load, (void *)ad);
+	evas_object_event_callback_add(ad->md.view_layout, EVAS_CALLBACK_RESIZE,
+			setting_applications_ug_cb_resize, ad);
+	return true;
 }
 
-static void setting_applications_ug_on_start(ui_gadget_h ug,
-		app_control_h service, void *priv)
-{
-}
-
-static void setting_applications_ug_on_pause(ui_gadget_h ug,
-		app_control_h service, void *priv)
+static void app_pause(void *data)
 {
 	SETTING_TRACE_BEGIN;
-	setting_retm_if((!priv), "!priv");
 }
 
-static void setting_applications_ug_on_resume(ui_gadget_h ug,
-		app_control_h service, void *priv)
+static void app_resume(void *priv)
 {
 	SETTING_TRACE_BEGIN;
 	setting_retm_if((!priv), "!priv");
 
 #if 0
-	SettingApplicationsUG *applicationsUG = priv;
+	SettingApplicationsUG *applicationsUG = data;
 	if (applicationsUG->data_home) {
 		char *appid = vconf_get_str(
 				VCONFKEY_SETAPPL_SELECTED_PACKAGE_NAME);
@@ -277,74 +259,31 @@ static void setting_applications_ug_on_resume(ui_gadget_h ug,
 #endif
 }
 
-static void setting_applications_ug_on_destroy(ui_gadget_h ug,
-		app_control_h service, void *priv)
+static void app_reset(app_control_h service, void *data)
+{
+	SETTING_TRACE_BEGIN;
+}
+
+static void app_terminate(void *priv)
 {
 	SETTING_TRACE_BEGIN;
 	setting_retm_if((!priv), "!priv");
-	SettingApplicationsUG *applicationsUG = priv;
+	SettingApplicationsUG *ad = priv;
 
 	/* fix flash issue for gallery */
-	evas_object_event_callback_del(applicationsUG->win_main_layout,
+	evas_object_event_callback_del(ad->md.view_layout,
 			EVAS_CALLBACK_RESIZE,
 			setting_applications_ug_cb_resize);
-	applicationsUG->ug = ug;
+	FREE(ad->uri);
 
-	FREE(applicationsUG->uri);
-
-	/* called when this shared gadget is terminated. similar with
-	 * app_exit */
-	if (&setting_view_applications_main == applicationsUG->view_to_load) {
-		setting_view_destroy(&setting_view_applications_main,
-				applicationsUG);
-	} else {
-		/* do nothing */
-	}
-
-	if (NULL != ug_get_layout(applicationsUG->ug)) {
-		evas_object_hide(
-				(Evas_Object *)ug_get_layout(
-						applicationsUG->ug));
-		evas_object_del(
-				(Evas_Object *)ug_get_layout(
-						applicationsUG->ug));
+	if (&setting_view_applications_main == ad->view_to_load) {
+		setting_view_destroy(&setting_view_applications_main, ad);
 	}
 
 	SETTING_TRACE_END;
 }
 
-static void setting_applications_ug_on_message(ui_gadget_h ug,
-		app_control_h msg, app_control_h service, void *priv)
-{
-	SETTING_TRACE_BEGIN;
-}
-
-static void setting_applications_ug_on_event(ui_gadget_h ug,
-		enum ug_event event, app_control_h service, void *priv)
-{
-	SETTING_TRACE_BEGIN;
-	SettingApplicationsUG *ad = priv;
-	setting_retm_if(NULL == ad, "ad is NULL");
-	switch (event) {
-	case UG_EVENT_LOW_MEMORY:
-		break;
-	case UG_EVENT_LOW_BATTERY:
-		break;
-	case UG_EVENT_LANG_CHANGE:
-		setting_navi_items_update(ad->navi_bar);
-		break;
-	case UG_EVENT_ROTATE_PORTRAIT:
-		break;
-	case UG_EVENT_ROTATE_LANDSCAPE:
-	case UG_EVENT_ROTATE_LANDSCAPE_UPSIDEDOWN:
-		break;
-	case UG_EVENT_REGION_CHANGE:
-		break;
-	default:
-		break;
-	}
-}
-
+/*
 static void setting_applications_ug_on_key_event(ui_gadget_h ug,
 		enum ug_key_event event, app_control_h service, void *priv)
 {
@@ -353,9 +292,9 @@ static void setting_applications_ug_on_key_event(ui_gadget_h ug,
 
 	switch (event) {
 	case UG_KEY_EVENT_END: {
-		if (elm_naviframe_top_item_get(ad->navi_bar)
+		if (elm_naviframe_top_item_get(ad->md.navibar_main)
 				== elm_naviframe_bottom_item_get(
-						ad->navi_bar)) {
+						ad->md.navibar_main)) {
 			ug_destroy_me(ug);
 		} else {
 			setting_view_cb_at_endKey(ad);
@@ -366,40 +305,53 @@ static void setting_applications_ug_on_key_event(ui_gadget_h ug,
 		break;
 	}
 }
+*/
 
-UG_MODULE_API int UG_MODULE_INIT(struct ug_module_ops *ops)
+static void _lang_changed(app_event_info_h event_info, void *data)
 {
 	SETTING_TRACE_BEGIN;
-	SettingApplicationsUG *applicationsUG = calloc(1,
-			sizeof(SettingApplicationsUG));
-	setting_retvm_if(!applicationsUG, -1,
-			"Create SettingApplicationsUG obj failed");
+	SettingApplicationsUG *ad = data;
+	setting_retm_if(NULL == ad, "ad is NULL");
 
-	memset(applicationsUG, 0x00, sizeof(SettingApplicationsUG));
-
-	ops->create = setting_applications_ug_on_create;
-	ops->start = setting_applications_ug_on_start;
-	ops->pause = setting_applications_ug_on_pause;
-	ops->resume = setting_applications_ug_on_resume;
-	ops->destroy = setting_applications_ug_on_destroy;
-	ops->message = setting_applications_ug_on_message;
-	ops->event = setting_applications_ug_on_event;
-	ops->key_event = setting_applications_ug_on_key_event;
-	ops->priv = applicationsUG;
-	ops->opt = UG_OPT_INDICATOR_ENABLE;
-
-	return 0;
+	char *lang = NULL;
+	if (app_event_get_language(event_info, &lang) == APP_ERROR_NONE) {
+		SETTING_TRACE_DEBUG("Setting - language is changed : %s", lang);
+		elm_language_set(lang);
+		elm_config_all_flush();
+		free(lang);
+		setting_navi_items_update(ad->md.navibar_main);
+	} else {
+		SETTING_TRACE_ERROR("Cannot get language from event_info");
+	}
 }
 
-UG_MODULE_API void UG_MODULE_EXIT(struct ug_module_ops *ops)
+EXPORT_PUBLIC
+int main(int argc, char *argv[])
 {
-	SETTING_TRACE_BEGIN;
-	struct SettingApplicationsUG *applicationsUG;
-	setting_retm_if(!ops, "ops == NULL");
+	app_event_handler_h handlers[5] = {NULL, };
+	ui_app_lifecycle_callback_s ops = {
+		.create = app_create,
+		.pause = app_pause,
+		.resume = app_resume,
+		.terminate = app_terminate,
+		.app_control = app_reset,
+	};
+	SettingApplicationsUG app_data;
 
-	applicationsUG = ops->priv;
-	if (applicationsUG)
-		FREE(applicationsUG);
+	ui_app_add_event_handler(&handlers[APP_EVENT_LOW_MEMORY],
+			APP_EVENT_LOW_MEMORY, NULL, NULL);
+	ui_app_add_event_handler(&handlers[APP_EVENT_LOW_BATTERY],
+			APP_EVENT_LOW_BATTERY, NULL, NULL);
+	ui_app_add_event_handler(&handlers[APP_EVENT_LANGUAGE_CHANGED],
+			APP_EVENT_LANGUAGE_CHANGED, _lang_changed, &app_data);
+	ui_app_add_event_handler(&handlers[APP_EVENT_REGION_FORMAT_CHANGED],
+			APP_EVENT_REGION_FORMAT_CHANGED, NULL, NULL);
+	ui_app_add_event_handler(
+			&handlers[APP_EVENT_DEVICE_ORIENTATION_CHANGED],
+			APP_EVENT_DEVICE_ORIENTATION_CHANGED, NULL, NULL);
+	memset(&app_data, 0x0, sizeof(app_data));
+
+	return ui_app_main(argc, argv, &ops, &app_data);
 }
 
 /* ***************************************************
@@ -408,107 +360,11 @@ UG_MODULE_API void UG_MODULE_EXIT(struct ug_module_ops *ops)
  *
  ***************************************************/
 
+/*
 #define BUS_NAME			"org.tizen.system.deviced"
 #define INTERFACE_NAME			BUS_NAME
 #define OBJECT_PATH			"/Org/Tizen/System/DeviceD"
 #define DEVICED_PATH_DISPLAY		OBJECT_PATH"/Applications"
 #define DEVICED_INTERFACE_DISPLAY	INTERFACE_NAME".applications"
 #define DBUS_REPLY_TIMEOUT		(120 * 1000)
-
-static UNUSED int __append_param(DBusMessage *msg, const char *sig,
-		char *param[])
-{
-	DBusMessageIter iter;
-	/*DBusMessageIter cont_iter; */
-	char *ch;
-	char *str_type;
-	int int_type;
-	int i;
-	if (sig == NULL || param == NULL)
-		return 0;
-
-	dbus_message_iter_init_append(msg, &iter);
-	for (ch = (char *)sig, i = 0; *ch != '\0'; ++i, ++ch) {
-		SETTING_TRACE_DEBUG("sig : %c", *ch);
-		switch (*ch) {
-		case 'i':
-			int_type = atoi(param[i]);
-			SETTING_TRACE_DEBUG("param[%2d] : %d", i, int_type);
-			dbus_message_iter_append_basic(&iter, DBUS_TYPE_INT32,
-					&int_type);
-			break;
-		case 's':
-			str_type = param[i];
-			SETTING_TRACE_DEBUG("param[%2d] : %s", i, str_type);
-			dbus_message_iter_append_basic(&iter, DBUS_TYPE_STRING,
-					&str_type);
-			break;
-		default:
-			break;
-		}
-	}
-	return 0;
-}
-
-static UNUSED DBusMessage *__invoke_dbus_method(const char *dest,
-		const char *path, const char *interface, const char *method,
-		const char *sig, char *param[])
-{
-	DBusError err;
-	DBusConnection *conn;
-	DBusMessage *reply;
-	DBusMessage *msg;
-	int r;
-
-	conn = dbus_bus_get(DBUS_BUS_SYSTEM, NULL);
-	if (conn == NULL) {
-		SETTING_TRACE_DEBUG("dbus_bus_get error");
-		return NULL;
-	}
-	msg = dbus_message_new_method_call(dest, path, interface, method);
-	if (msg == NULL) {
-		SETTING_TRACE_DEBUG("dbus_message_new_method_call error");
-		return NULL;
-	}
-	r = __append_param(msg, sig, param);
-	if (r < 0) {
-		SETTING_TRACE_DEBUG("append_param error");
-		return NULL;
-	}
-	dbus_error_init(&err);
-	reply = dbus_connection_send_with_reply_and_block(conn, msg,
-			DBUS_REPLY_TIMEOUT, &err);
-	dbus_message_unref(msg);
-	if (dbus_error_is_set(&err)) {
-		SETTING_TRACE_DEBUG(
-				"dbus_connection_send_with_reply_and_block error");
-		SETTING_TRACE_DEBUG("error [%s:%s]", err.name, err.message);
-		dbus_error_free(&err);
-		return NULL;
-	}
-	return reply;
-}
-
-void setting_applications_layout_ug_cb(ui_gadget_h ug, enum ug_mode mode,
-		void *priv)
-{
-	SETTING_TRACE_BEGIN;
-	/*SettingApplicationsUG *ad = (SettingApplicationsUG *) priv; */
-	Evas_Object *base;
-	ret_if(!priv);
-
-	base = (Evas_Object *)ug_get_layout(ug);
-	ret_if(!base);
-
-	switch (mode) {
-	case UG_MODE_FULLVIEW:
-		evas_object_size_hint_weight_set(base, EVAS_HINT_EXPAND,
-		EVAS_HINT_EXPAND);
-		evas_object_show(base);
-		break;
-	default:
-		break;
-	}
-
-	SETTING_TRACE_END;
-}
+*/
